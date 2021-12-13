bdaf46248f150de131f863ffacd9b32d054bada0
thamo2020 <thamodya01@gmail.com>
2021-12-13 22:44:49 +0530

    commit 2

---
+++ src/components/SingleContent/SingleContent.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const SingleContent = () => {
+    return (
+        <div>
+            const SingleContent = ({
+                id,
+                poster,
+                title,
+                date,
+                media_type,
+                vote_average,
+        
+    )
+}
+</div>
+export default SingleContent
