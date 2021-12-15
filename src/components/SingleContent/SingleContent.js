72f7cf89e72d05703a629fd646ba4b7b1e9f4a99
thamo2020 <thamodya01@gmail.com>
2021-12-15 11:27:24 +0530

    adding badge

--- src/components/SingleContent/SingleContent.js
+++ src/components/SingleContent/SingleContent.js
@@ -1,3 +1,4 @@
+import { Badge } from '@material-ui/core';
 import React from 'react'
 import { img_300, unavailable } from '../../config/config';
 import './SingleContent.css' ;
@@ -16,7 +17,7 @@ const SingleContent = ( {
  
     return ( 
     <div className='media'>
-
+      <Badge   badgeContent={vote_average} color={vote_average>6 ? 'primary':'secondary'}/>
         <img className='poster' 
         src={poster ? `${img_300}/${poster}` : unavailable }
         />
